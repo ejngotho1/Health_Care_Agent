6ab87491d0b65196ac9c5076ae79d5286650acdf
Edgar <edgar.ngotho@gmail.comgit config --global user.name Edgargit config --global user.email edgar.ngotho@gmail.com>
2026-01-05 13:25:18 -0800

    folder structure added

---
+++ project.sh
@@ -0,0 +1,18 @@
+# creating directories for the project
+mkdir -p src
+mkdir -p bin
+mkdir -p research
+mkdir -p docs
+mkdir -p tests
+
+# creating initial files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch app.py
+touch research/README.md
+touch reserch/trials.ipynb
+touch requirements.txt
+
+echo "Project structure created successfully."
